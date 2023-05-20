e76587c39c04ea88b7aaddd0f920c33154184a77
Grayni <grayniweb@gmail.com>
2023-05-20 04:37:53 +0300

    fix cursor behavior

--- cursor_manager.py
+++ cursor_manager.py
@@ -1,6 +1,5 @@
 class CursorManager:
     def __init__(self):
-        self.root = None
         self.arrow_path = "@assets/cursors/arrow.cur"
         self.arrow_minus_path = "@assets/cursors/arrow_minus.cur"
         self.hand_path = "@assets/cursors/hand.cur"
@@ -13,37 +12,29 @@ class CursorManager:
         pass
 
     def arrow_cursor(self):
+        self.previous_cursor = self.cget("cursor")
         self.configure(cursor=self.arrow_path)
 
-    def arrow_minus_cursor(self):
-        self.configure(cursor=self.arrow_minus_path)
-
     def hand_cursor(self, event):
+        self.previous_cursor = self.cget("cursor")
         self.configure(cursor=self.hand_path)
 
     def drag_cursor(self):
         self.configure.configure(cursor=self.drag_path)
 
     def cross_cursor(self):
+        self.previous_cursor = self.cget("cursor")
         self.configure(cursor="crosshair")
 
     def delete_cursor(self):
         self.configure(cursor=self.arrow_minus_path)
 
     def restore_cursor(self, event):
-        self.root.configure(cursor=self.default_cursor)
+        self.configure(cursor=self.previous_cursor)
 
     def ctrl_ready(self, event):
         if self.allow_influence_point(event):
             self.delete_cursor()
-
-    def ctrl_out(self, event):
-        if self.allow_influence_point(event):
-            self.hand_cursor(event)
-        elif self.allow_add_point(event):
-            self.cross_cursor()
-        else:
-            self.arrow_cursor()
 
 
 
